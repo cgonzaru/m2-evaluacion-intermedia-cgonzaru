357bad6b8180ac67cd61051568fc95d3fed446fa
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-29 09:04:52 +0200

    input value

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@
 const button = document.querySelector('.btn');
 const check = document.querySelector('.check__input');
 const solution = document.querySelector('.solution__text');
+const counter = document.querySelector('.counter');
 
 // Función
 function guess () {
     console.log('ok');
+    
+    const write = check.value;
+    console.log('el valor del campo es:', write);
 }
 
 // Eventos
