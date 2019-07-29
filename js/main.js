fa66dbb0e9cd43ddca260dcfe74e12cc4a601ef4
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-29 08:53:16 +0200

    add event button

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Elementos del HTML
+const button = document.querySelector('.btn');
+const check = document.querySelector('.check__input');
+const solution = document.querySelector('.solution__text');
+
+// Función
+function guess () {
+    console.log('ok');
+}
+
+// Eventos
+button.addEventListener('click', guess);
