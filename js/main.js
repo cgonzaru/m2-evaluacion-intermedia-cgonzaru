6e3633ee89bb631715f40ba37dfd12fa66c5fbef
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-29 09:23:44 +0200

    event function

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Elementos del HTML
 const button = document.querySelector('.btn');
 const check = document.querySelector('.check__input');
-const solution = document.querySelector('.solution__text');
+let solutionText = document.querySelector('.solution__text');
 const counter = document.querySelector('.counter');
 
 // Función
@@ -11,7 +11,19 @@ function guess () {
     console.log('ok');
     
     const write = check.value;
+    
     console.log('el valor del campo es:', write);
+    
+    // si el número introducido es mayor que el random (suponemos 10)
+    if (write > 10) { 
+        solutionText.innerHTML = `Demasiado alto`;
+        console.log('Demasiado alto');
+    } else if (write < 10) {
+        solutionText.innerHTML = `Demasiado bajo`;
+        console.log('Demasiado bajo');
+    } else {
+        solutionText.innerHTML = `¡Has ganado campeona!`;
+    }
 }
 
 // Eventos
